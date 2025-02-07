03a14fdc4988ddf1af2f1a8cd9c3dae1abdd4607
Chien Tran <minhchien.arashino@gmail.com>
2025-02-07 20:29:15 +0700

    Init project

---
+++ play-github-action/src/main/java/project/playgithubaction/PlayGithubActionApplication.java
@@ -0,0 +1,13 @@
+package project.playgithubaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayGithubActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayGithubActionApplication.class, args);
+    }
+
+}
---
+++ play-github-action/src/test/java/project/playgithubaction/PlayGithubActionApplicationTests.java
@@ -0,0 +1,13 @@
+package project.playgithubaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayGithubActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
